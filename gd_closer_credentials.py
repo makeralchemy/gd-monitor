9e42e22e344a3ed4748b5fc54794e9852efaaf8b
cyber alchemist <talk2bruce@makeralchemy.com>
2023-07-02 16:31:33 -0700

    Adding code to support door closing device
    Credentials file for sending http requests to the Particle Photon device that controls the garage door closing.

---
+++ gd_closer_credentials.py
@@ -0,0 +1,3 @@
+# Credentials for the Particle Photon garage door closer device
+GD_CLOSER_BEARER = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" # authorization token
+GD_CLOSER_DEVICE = "dddddddddddddddddddddddd"                 # device id
