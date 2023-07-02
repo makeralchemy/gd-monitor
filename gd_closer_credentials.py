f561797597e136e931f8a115468ab8310eb60bb3
cyber alchemist <talk2bruce@makeralchemy.com>
2023-07-02 16:34:00 -0700

    Updated comments

--- gd_closer_credentials.py
+++ gd_closer_credentials.py
@@ -1,3 +1,4 @@
 # Credentials for the Particle Photon garage door closer device
+# Replace with the values from the Particle Photon and Particle Cloud
 GD_CLOSER_BEARER = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" # authorization token
 GD_CLOSER_DEVICE = "dddddddddddddddddddddddd"                 # device id
